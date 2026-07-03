cd8ccdb6444b612ee9de7c1d5c1ecbf10bbe4bf1
StanNAlexandru <alexnstan@gmail.com>
2026-07-03 08:21:12 +0300

    Add product actions interface

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -19,6 +19,14 @@
             <p class="product__name">{{ $product->name }}</p>
             <p>{{ $product->price }}$</p>
             <p>{{ $product->description }}</p>
+            <div class="product__actions">
+                <a href="#" class="button button--success">Edit</a>
+                <form action="" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="button button--danger">Delete</button>
+                </form>
+            </div>
         </div>
     </div>
 @endforeach
